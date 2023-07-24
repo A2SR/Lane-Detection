6595f01313d42fcdadda81e57e8a2bbe23efe883
BigBull21 <arhino98@gmail.com>
2023-07-24 00:03:19 -0400

    Update lanedetection.py

--- lanedetection.py
+++ lanedetection.py
@@ -1,43 +1,75 @@
 import cv2
 import numpy as np
 
+MIN_LINE_LENGTH = 250
 
-def detect_lanes_hough(image):
-    # Convert the image to grayscale
+
+class Image:
+    def __init__(self):
+        self.mask = " "
+        self.lanes = " "
+
+
+def detect_lanes_hough():
+
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Apply Canny edge detection
     edges = cv2.Canny(gray_image, 50, 150)
 
-    # Apply Hough Transform to detect lines
-    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi /
-                            180, threshold=50, minLineLength=100, maxLineGap=5)
+    # ROI Mask
+    height, width = edges.shape
+    roi_mask = np.zeros_like(edges)
+    roi_vertices = np.array([[(300, height), (width // 2, height // 2 + 250),
+                             (width // 2 + 100, height // 2 + 250), (width - 300, height)]], dtype=np.int32)
+    cv2.fillPoly(roi_mask, roi_vertices, 255)
+    masked_edges = cv2.bitwise_and(edges, roi_mask)
+    roi_color = (0, 255, 0)  # Green color for the ROI mask
+    mask = cv2.polylines(
+        image.copy(), [roi_vertices], isClosed=True, color=roi_color, thickness=2
+    )
 
-    # Create a blank image to draw the detected lane lines
+    # Detect lines
+    lines = cv2.HoughLinesP(masked_edges, rho=1, theta=np.pi /
+                            180, threshold=50, minLineLength=MIN_LINE_LENGTH, maxLineGap=200)
+
+    # Filter lines
+    filtered_lines = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        slope = (y2 - y1) / (x2 - x1)
+
+        # Filter based on slope and length
+        min_slope_threshold = 0.3
+        min_length_threshold = 450
+        if abs(slope) > min_slope_threshold and np.sqrt((x2 - x1)**2 + (y2 - y1)**2) > min_length_threshold:
+            filtered_lines.append(line)
+
     lane_lines_image = np.zeros_like(image)
 
-    # Draw the lane lines on the blank image
-    for line in lines:
+    # Draw Lane Lines
+    for line in filtered_lines:
         x1, y1, x2, y2 = line[0]
         cv2.line(lane_lines_image, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
     # Combine the original image with the detected lane lines
-    result_image = cv2.addWeighted(image, 0.8, lane_lines_image, 1, 0)
+    lanes = cv2.addWeighted(image, 0.8, lane_lines_image, 1, 0)
 
-    return result_image
+    Display = Image()
+    Display.mask = mask
+    Display.image = lanes
+
+    return Display
 
 
-# Read an example road image
 image = cv2.imread(
     "/Users/aaronrinehart/Downloads/shutterstock_1279312912-scaled.jpg")
 
-# Call the lane detection function
-result = detect_lanes_hough(image)
+result = detect_lanes_hough()
 
-# Display the original and result images
-cv2.imshow("Original Image", image)
-cv2.imshow("Lane Detection Result", result)
+# Display the mask and the lane line image
+cv2.imshow("Original Image with ROI Mask", result.mask)
+cv2.waitKey(0)
+cv2.imshow("Lane Detection Result", result.image)
 
-# Wait for a key press and close the windows
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
